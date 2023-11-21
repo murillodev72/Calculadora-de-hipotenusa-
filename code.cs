2ddf715105324eab55174a25a72986909419ca9d
murillo dev72 <148728763+murillodev72@users.noreply.github.com>
2023-11-21 15:24:58 -0300

    Add files via upload

---
+++ code.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace CalculadoraDeHipotenusa{
+class classe{
+	static void Main(String[] args){
+		
+	Console.WriteLine("informe o lado A: ");
+	double A = Convert.ToDouble(Console.ReadLine());
+	
+	Console.WriteLine("informe o lado B: ");
+	double B = Convert.ToDouble(Console.ReadLine());
+	
+	double C = Math.Sqrt((A*A) + (B*B));
+	Console.WriteLine(" a hipotenusa é: "+ C);
+	
+	}
+}
+}
